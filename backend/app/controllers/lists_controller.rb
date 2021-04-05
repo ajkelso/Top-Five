f25dcde1d677b4d689509d261a0734f9ee7b19f2
ajkelso <ajkelso@gmail.com>
2021-04-05 16:09:34 -0400

    build list serializer

--- backend/app/controllers/lists_controller.rb
+++ backend/app/controllers/lists_controller.rb
@@ -4,7 +4,6 @@ class ListsController < ApplicationController
 
         category = Category.find_or_create_by(title: params[:category])
         new_list = List.create(user_id: current_user.id, category_id: category.id)
-        byebug
         new_list.first = Nomination.find_or_create_by(name: params["first"], category_id: category.id).id
         new_list.second = Nomination.find_or_create_by(name: params["second"], category_id: category.id).id
         new_list.third = Nomination.find_or_create_by(name: params["third"], category_id: category.id).id
--- backend/app/serializers/list_serializer.rb
+++ backend/app/serializers/list_serializer.rb
@@ -1,3 +1,14 @@
 class ListSerializer < ActiveModel::Serializer
-  attributes :id, :first, :second, :third, :fourth, :fifth
+  attributes :id, :top_five
+
+  def top_five
+    {
+      category: Category.find(object.category_id).title,
+      first: Nomination.find(object.first).name,
+      second: Nomination.find(object.second).name,
+      third: Nomination.find(object.third).name,
+      fourth: Nomination.find(object.fourth).name,
+      fifth: Nomination.find(object.fifth).name
+    }
+  end
 end
