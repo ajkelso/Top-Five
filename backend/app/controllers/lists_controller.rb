ef7d78a27f59f6174b29c88e0c84a2915698bdbe
ajkelso <ajkelso@gmail.com>
2021-04-07 12:31:06 -0400

    adjust vote increase/serialization

--- backend/app/controllers/lists_controller.rb
+++ backend/app/controllers/lists_controller.rb
@@ -9,12 +9,12 @@ class ListsController < ApplicationController
         third = Nomination.find_or_create_by(name: params["third"], category_id: category.id)
         fourth = Nomination.find_or_create_by(name: params["fourth"], category_id: category.id)
         fifth = Nomination.find_or_create_by(name: params["fifth"], category_id: category.id)
-        
-        first.increment(:votes, 5)
-        second.increment(:votes, 4)
-        third.increment(:votes, 3)
-        fourth.increment(:votes, 2)
-        fifth.increment(:votes, 1)
+        byebug
+        first.increment!(:votes, 5)
+        second.increment!(:votes, 4)
+        third.increment!(:votes, 3)
+        fourth.increment!(:votes, 2)
+        fifth.increment!(:votes, 1)
         
         new_list.update(first: first.id, second: second.id, third: third.id, fourth: fourth.id, fifth:fifth.id)  
 
--- backend/app/serializers/categories_serializer.rb
+++ backend/app/serializers/categories_serializer.rb
@@ -1,8 +1,8 @@
 class CategoriesSerializer < ActiveModel::Serializer
-  attributes :id :title
+  attributes :id, :title, :top_ten
 
   def top_ten
-    
+    Nomination.filter_by_category(object.id).desc_order.limit(10)
   end
 
 end
