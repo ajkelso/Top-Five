b09bd6e202a7683e3fa28e3b27c75b122081fd93
ajkelso <ajkelso@gmail.com>
2021-04-08 16:37:31 -0400

    make form horizontal

--- frontend/src/components/ListForm.jsx
+++ frontend/src/components/ListForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addUserList } from '../redux/actions/userActions'
 import { useHistory } from "react-router-dom"
-import Form from 'react-bootstrap/Form'
+import { Form, Row, Col } from 'react-bootstrap'
 
 function ListForm(props) {
 
@@ -39,40 +39,50 @@ function ListForm(props) {
 
             <h4>Create a top 5 List</h4>
 
-            <form onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="listForm.category">
                     <Form.Control type="text" name="category" placeholder="Choose a Category..." onChange={handleChange} value={formData.category}/>
                 </Form.Group>
-                <Form.Group controlId="listForm.First">
-                    <Form.Label htmlFor="First">1.</Form.Label>
-                    <Form.Control type="text" name="first" onChange={handleChange} value={formData.first}/><br/>
+                <Form.Group as={Row} controlId="listForm.First">
+                    <Form.Label column sm={1} htmlFor="First">1.</Form.Label>
+                    <Col sm={10}>
+                        <Form.Control type="text" name="first" onChange={handleChange} value={formData.first}/><br/>
+                    </Col>
                 </Form.Group>
 
-                <Form.Group controlId="listForm.Second">
-                    <Form.Label htmlFor="Second">2.</Form.Label>
-                    <Form.Control type="text" name="second" onChange={handleChange} value={formData.second}/><br/>
+                <Form.Group as={Row} controlId="listForm.Second">
+                    <Form.Label column sm={1} htmlFor="Second">2.</Form.Label>
+                    <Col sm={10}>
+                        <Form.Control type="text" name="second" onChange={handleChange} value={formData.second}/><br/>
+                    </Col>
                 </Form.Group>
 
-                <Form.Group controlId="listForm.Third">
-                    <Form.Label htmlFor="Third">3.</Form.Label>
-                    <Form.Control type="text" name="third" onChange={handleChange} value={formData.third}/><br/>
+                <Form.Group as={Row} controlId="listForm.Third">
+                    <Form.Label column sm={1} htmlFor="Third">3.</Form.Label>
+                    <Col sm={10}>
+                        <Form.Control type="text" name="third" onChange={handleChange} value={formData.third}/><br/>
+                    </Col>
                 </Form.Group>
 
-                <Form.Group controlId="listForm.Fourth">
-                    <Form.Label htmlFor="Fourth">4.</Form.Label>
-                    <Form.Control type="text" name="fourth" onChange={handleChange} value={formData.fourth}/><br/>
+                <Form.Group as={Row} controlId="listForm.Fourth">
+                    <Form.Label column sm={1} htmlFor="Fourth">4.</Form.Label>
+                    <Col sm={10}>
+                        <Form.Control type="text" name="fourth" onChange={handleChange} value={formData.fourth}/><br/>
+                    </Col>
                 </Form.Group>
 
-                <Form.Group controlId="listForm.Fifth">
-                    <Form.Label htmlFor="Fifth">5.</Form.Label>
-                    <Form.Control type="text" name="fifth" onChange={handleChange} value={formData.fifth}/><br/>
+                <Form.Group as={Row} controlId="listForm.Fifth">
+                    <Form.Label column sm={1} htmlFor="Fifth">5.</Form.Label>
+                    <Col sm={10}>
+                        <Form.Control type="text" name="fifth" onChange={handleChange} value={formData.fifth}/><br/>
+                    </Col>
                 </Form.Group>
 
-                <Form.Group controlId="listForm.Submit">
+                <Form.Group as={Row} controlId="listForm.Submit">
                     <Form.Control type="submit"/>
                 </Form.Group>
 
-            </form>
+            </Form>
         </div>
     )
 }
