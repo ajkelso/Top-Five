a19018676e513738b6554bfb98e53e71262e0ffb
ajkelso <ajkelso@gmail.com>
2021-04-08 16:24:34 -0400

    add bootstrap to form

--- frontend/src/components/ListForm.jsx
+++ frontend/src/components/ListForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addUserList } from '../redux/actions/userActions'
 import { useHistory } from "react-router-dom"
+import Form from 'react-bootstrap/Form'
 
 function ListForm(props) {
 
@@ -39,18 +40,38 @@ function ListForm(props) {
             <h4>Create a top 5 List</h4>
 
             <form onSubmit={handleSubmit}>
-                <input type="text" name="category" placeholder="Choose a Category..." onChange={handleChange} value={formData.category}/><br/>
-                <label htmlFor="First">1.</label>
-                <input type="text" name="first" onChange={handleChange} value={formData.first}/><br/>
-                <label htmlFor="Second">2.</label>
-                <input type="text" name="second" onChange={handleChange} value={formData.second}/><br/>
-                <label htmlFor="Third">3.</label>
-                <input type="text" name="third" onChange={handleChange} value={formData.third}/><br/>
-                <label htmlFor="Fourth">4.</label>
-                <input type="text" name="fourth" onChange={handleChange} value={formData.fourth}/><br/>
-                <label htmlFor="Fifth">5.</label>
-                <input type="text" name="fifth" onChange={handleChange} value={formData.fifth}/><br/>
-                <input type="submit"/>
+                <Form.Group controlId="listForm.category">
+                    <Form.Control type="text" name="category" placeholder="Choose a Category..." onChange={handleChange} value={formData.category}/>
+                </Form.Group>
+                <Form.Group controlId="listForm.First">
+                    <Form.Label htmlFor="First">1.</Form.Label>
+                    <Form.Control type="text" name="first" onChange={handleChange} value={formData.first}/><br/>
+                </Form.Group>
+
+                <Form.Group controlId="listForm.Second">
+                    <Form.Label htmlFor="Second">2.</Form.Label>
+                    <Form.Control type="text" name="second" onChange={handleChange} value={formData.second}/><br/>
+                </Form.Group>
+
+                <Form.Group controlId="listForm.Third">
+                    <Form.Label htmlFor="Third">3.</Form.Label>
+                    <Form.Control type="text" name="third" onChange={handleChange} value={formData.third}/><br/>
+                </Form.Group>
+
+                <Form.Group controlId="listForm.Fourth">
+                    <Form.Label htmlFor="Fourth">4.</Form.Label>
+                    <Form.Control type="text" name="fourth" onChange={handleChange} value={formData.fourth}/><br/>
+                </Form.Group>
+
+                <Form.Group controlId="listForm.Fifth">
+                    <Form.Label htmlFor="Fifth">5.</Form.Label>
+                    <Form.Control type="text" name="fifth" onChange={handleChange} value={formData.fifth}/><br/>
+                </Form.Group>
+
+                <Form.Group controlId="listForm.Submit">
+                    <Form.Control type="submit"/>
+                </Form.Group>
+
             </form>
         </div>
     )
