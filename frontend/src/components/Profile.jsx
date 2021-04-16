b016d719ffa68deeee99e8ded57f76faa7807b09
ajkelso <ajkelso@gmail.com>
2021-04-16 16:08:53 -0400

    cadd button link to new lists in Profile

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getUser } from '../redux/actions/userActions'
 import ListContainer from './ListContainer'
+import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
 
 function Profile() {
 
@@ -17,7 +19,7 @@ function Profile() {
         <div>
             <h2 className="text-center">Welcome, {user.username}!</h2>
            
-            <ListContainer lists={user.lists} />
+            {user.lists.length ? <ListContainer lists={user.lists} /> : <Link className="btn btn-info btn-lg d-flex justify-content-center" to="/lists/new">Start Creating Lists!</Link>}
         </div>
     )
 }
